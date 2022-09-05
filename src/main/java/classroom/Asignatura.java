d3401a74dc2c2d051e42ff14498514cbd612877d
jomolinav <111089102+jomolinav@users.noreply.github.com>
2022-09-05 16:53:41 -0500

    Cambio1

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,15 +10,10 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
     Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
-
-    Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+        this("Sin nombre", codigoInterno, codigoInterno,tipo);
     }
 
     Asignatura(String nombre, Tipo tipo) {
@@ -37,10 +32,6 @@ public class Asignatura {
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
         this.tipo = tipo;
-    }
-
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
     }
 
     void cambiarDatos(int codigoExterno) {
